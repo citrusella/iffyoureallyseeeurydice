02ec10c68a9b531150bc6c3e7be0d8141a1ea1a6
citrusella <purplewowies@yahoo.com>
2026-01-07 21:02:33 -0500

    title bar icon

--- composeApp/src/jvmMain/kotlin/io/github/citrusella/iffyoureallyseeeurydice/main.kt
+++ composeApp/src/jvmMain/kotlin/io/github/citrusella/iffyoureallyseeeurydice/main.kt
@@ -4,7 +4,9 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import iffyoureallyseeeurydice.composeapp.generated.resources.Res
 import iffyoureallyseeeurydice.composeapp.generated.resources.app_name
+import iffyoureallyseeeurydice.composeapp.generated.resources.eurydice_icon_small
 import io.github.vinceglb.filekit.FileKit
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 fun main() {
@@ -13,6 +15,7 @@ fun main() {
         Window(
             onCloseRequest = ::exitApplication,
             title = stringResource(Res.string.app_name),
+            icon = painterResource(Res.drawable.eurydice_icon_small)
         ) {
             App()
         }
