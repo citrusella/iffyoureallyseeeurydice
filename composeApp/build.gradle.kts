348e8dced154872006d82d1f8b7efe71fccd11c9
citrusella <purplewowies@yahoo.com>
2026-03-08 15:17:03 -0400

    adjustment to known issue, increment version to account for new quick patch

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,7 +43,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe, TargetFormat.AppImage)
             packageName = "IFFYouReallySeeEurydice"
-            packageVersion = "1.0.1"
+            packageVersion = "1.0.2"
             description = "A simple tool for converting 1.0 version Sims iff files to 2.0"
             copyright = "Created in 2026 by purplewowies, released under GNU Public License 3.0"
             licenseFile.set(project.file("LICENSE.txt"))
